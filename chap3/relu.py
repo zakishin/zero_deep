6c862afc57e27a4245adb907f2699accc34f4dc4
zakishin <shinya.i0620@gmail.com>
2021-08-20 22:32:06 +0900

    chap3

---
+++ chap3/relu.py
@@ -0,0 +1,3 @@
+import numpy as np
+def relu(x):
+    return np.maximum(0,x)
---
+++ chap3/sigmoid.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import step_function
+import relu
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+x=np.arange(-5.0,5.0,0.1)
+y1=sigmoid(x)
+y2=step_function.step_function(x)
+y3=relu.relu(x)
+plt.plot(x,y1)
+plt.plot(x,y2,linestyle='--')
+plt.plot(x,y3,linestyle='dashdot')
+plt.ylim(-0.1,1.1)
+plt.show()
