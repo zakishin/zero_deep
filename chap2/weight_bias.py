d8a0d85f0633f80f41870537ee4c65052ab41c30
zakishin <shinya.i0620@gmail.com>
2021-08-20 21:43:29 +0900

    chap2

--- chap2/weight_bias.py
+++ chap2/weight_bias.py
@@ -1,9 +1,37 @@
 import numpy as np
-x=np.array([0,1])
-w=np.array([0.5,0.5])
-b=-0.7
-tmp=np.sum(w*x)+b
-if tmp>=0:
-    print(1)
-else:
-    print(0)
+from numpy.core.numeric import NaN
+def AND(x1,x2):
+    x=np.array([x1,x2])
+    w=np.array([0.5,0.5])
+    b=-0.7
+    tmp=np.sum(x*w)+b
+    if tmp<=0:
+        return 0
+    else:
+        return 1
+def NAND(x1,x2):
+    x=np.array([x1,x2])
+    w=np.array([-0.5,-0.5])
+    b=0.7
+    tmp=np.sum(x*w)+b
+    if tmp<=0:
+        return 0
+    else:
+        return 1
+def OR(x1,x2):
+    x=np.array([x1,x2])
+    w=np.array([0.5,0.5])
+    b=-0.2
+    tmp=np.sum(x*w)+b
+    if tmp<=0:
+        return 0
+    else:
+        return 1
+def XOR(x1,x2):
+    s1=NAND(x1,x2)
+    s2=OR(x1,x2)
+    y=AND(s1,s2)
+    return y
+
+
+print(AND(0,0))
